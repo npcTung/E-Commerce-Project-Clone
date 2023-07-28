4974dd7f298609bf3baebfd92d2d52992e14c254
npcTung <tungtrinhthanh0311@gmail.com>
2023-07-28 16:23:21 +0700

    2# Handle error

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 require("dotenv").config();
+const dbConnection = require("./config/dbConnect");
+const initRouter = require("./routers");
 
 const app = express();
 const port = process.env.PORT || 8888;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+dbConnection();
+initRouter(app);
 
 app.use("/", (rep, res) => {
   res.send("Server on...");
